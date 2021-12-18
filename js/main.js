ae2deebd8951917811191d97bd683aef87f2e171
Cyril Suzat <cyril.suzat@me.com>
2021-12-18 10:27:34 +0100

    Add backdrop to menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-function toggleNavbar() {
-  document.getElementById('menu').classList.toggle('visible');
+function toggleMenu() {
+  let menu = document.getElementById('menu');
+
+  if (menu.classList.contains('visible')) {
+    menu.classList.remove('visible');
+
+    let backdrop = document.getElementById('menu-backdrop');
+    backdrop.classList.remove('visible');
+    setTimeout(() => { backdrop.remove(); }, 300);
+  } else {
+    menu.classList.add('visible');
+
+    let backdrop = document.createElement('div');
+    backdrop.id = 'menu-backdrop';
+    backdrop.onclick = () => { return toggleMenu(); };
+    let navbar = document.getElementById('navbar');
+    navbar.after(backdrop);
+    backdrop.focus();
+    backdrop.classList.add('visible');
+  }
+
   return false;
 }
