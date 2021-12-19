d2485fc80e0ed6e65306a9c7f8588ae5fe29607b
Cyril Suzat <cyril.suzat@me.com>
2021-12-19 10:56:48 +0100

    Add images transition with fading on home hero

--- js/main.js
+++ js/main.js
@@ -26,5 +26,60 @@ function toggleMenu() {
 // Hero - Parallax effect
 window.addEventListener('scroll', function() {
   const distance = window.scrollY
-  document.querySelector('.hero img').style.transform = `translateY(${distance * 0.2}px)`
+  document.querySelector('.hero-img').style.transform = `translateY(${distance * 0.2}px)`
 })
+
+// Hero - Image transition with fading
+startImageTransition();
+
+function startImageTransition() {
+  var images = document.getElementsByClassName('img-slide');
+
+  for (var i = 0; i < images.length; ++i) {
+    images[i].style.opacity = 1;
+  }
+
+  var top = 1;
+
+  var cur = images.length - 1;
+
+  setInterval(changeImage, 3000);
+
+  async function changeImage() {
+
+    var nextImage = (1 + cur) % images.length;
+
+    images[cur].style.zIndex = top + 1;
+    images[nextImage].style.zIndex = top;
+
+    await transition();
+
+    images[cur].style.zIndex = top;
+
+    images[nextImage].style.zIndex = top + 1;
+
+    top = top + 1;
+
+    images[cur].style.opacity = 1;
+
+    cur = nextImage;
+
+  }
+
+  function transition() {
+    return new Promise(function(resolve, reject) {
+      var del = 0.01;
+
+      var id = setInterval(changeOpacity, 10);
+
+      function changeOpacity() {
+        images[cur].style.opacity -= del;
+        if (images[cur].style.opacity <= 0) {
+          clearInterval(id);
+          resolve();
+        }
+      }
+
+    })
+  }
+}
